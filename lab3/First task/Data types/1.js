de675fcd065c6a4b2367e2352f1f777633d7d99c
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-02-13 00:24:17 +0600

    git commit

---
+++ lab3/First task/Data types/1.js
@@ -0,0 +1,5 @@
+'use strict';
+let name = "Ilya";
+alert( `hello ${1}` ); // hello 1
+alert( `hello ${"name"}` ); // Hello name
+alert( `hello ${name}` ); // Hello Ilya
---
+++ lab3/First task/Variable/3.js
@@ -0,0 +1,3 @@
+'use strict';
+const BIRTHDAY = '21.01.2004'
+const age = someCode(BIRTHDAY);

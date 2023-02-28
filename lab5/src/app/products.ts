b4fc00c027395856c511ecf506acc63a04777f01
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-02-28 11:36:36 +0600

    added elements to 3 components

--- lab5/src/app/products.ts
+++ lab5/src/app/products.ts
@@ -47,17 +47,6 @@ export const products = [
   {
     id: 4,
     category: 1,
-    like: 7,
-    name: 'Dr. Jart Dermaclear Micro pH ',
-    price: 4899,
-    description: 'A perfectly balanced cleanser with a neutral pH of 5.5 is suitable for the most sensitive skin, gently, but at the same time thoroughly cleansing it. The gel, when in contact with water, transforms into a light foam.',
-    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc8/h68/31800021680158/dr-jart-dermaclear-micro-ph-foam-21900269-3-Container.jpg',
-    link: 'https://kaspi.kz/shop/p/dr-jart-dermaclear-micro-ph-foam-gel-120-ml-21900269/?c=750000000#!/item',
-    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg'
-  },
-  {
-    id: 5,
-    category: 1,
     like: 10,
     name: 'GUCCI Bloom Nettare Di Fiori perfume water',
     price: 86403,
@@ -67,29 +56,7 @@ export const products = [
     rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg'
   },
   {
-    id: 6,
-    category: 1,
-    like: 6,
-    name: 'Powder Dior Forever Cushion Powder crumbly 20 Light Beige',
-    price: 39899,
-    description: 'Ultra-thin refreshing loose powder is a persistent translucent perfection of the First* loose powder that fixes makeup and gives the skin a light, velvety glow without powdery effect. Housed in a travel-friendly couture case, this loose powder with a fresh and silky texture harmonizes with the skin tone, providing an even, matte and naturally radiant complexion in any situation. This loose powder, water-based and extract of wild pansies, gently cares for the skin and improves the complexion. Tested by dermatologists - does not cause comedones.',
-    link: 'https://kaspi.kz/shop/p/dior-forever-cushion-powder-rassypchataja-20-light-bezhevyi-103939007/?c=750000000#!/item',
-    rating: 'https://t4.ftcdn.net/jpg/02/74/86/43/360_F_274864312_uNlm9yCpdViwKzHkCp0sOBrmJFN0pKAa.jpg',
-    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h2e/hf3/49086897782814/diorskin-forever-cushion-powder-pudra-kuson-20-svetlyj-103939007-1.jpg'
-  },
-  {
-    id: 7,
-    category: 1,
-    like: 6,
-    name: 'Dior Backstage Face Body Foundation Foundation Cream 1.5 50 ml',
-    price: 30000,
-    description: 'The Dior Backstage Face and Body foundation is the secret weapon of Dior makeup artists to instantly create a flawless complexion. With its help, you can control the intensity of the coating — from shining nude to saturated.',
-    link: 'https://kaspi.kz/shop/p/dior-backstage-face-body-foundation-tonal-nyi-krem-1-5n-50-ml-104108912/?c=750000000#!/item',
-    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
-    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h2d/hbf/49360691593246/backstage-face-body-foundation-104108912-1.jpg'
-  },
-  {
-    id: 8,
+    id: 5,
     category: 1,
     like: 6,
     name: 'Dior Sauvage perfume water EDP 100 ml',
@@ -100,27 +67,115 @@ export const products = [
     image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hf7/hfb/32602046038046/christian-dior-sauvage-edp-100-ml-100178749-1-Container.jpg'
   },
   {
-    id: 9,
-    category: 1,
-    like: 6,
-    name: 'Estee Lauder Advanced Night Repair Facial Serum 50 ml',
-    price: 50363,
-    description: 'An innovative means of a new generation — Advanced Night Repair serum with a complex action. Enriched with Chronolux ™ Power Signal technology, this serum is deeply and quickly absorbed into the skin. It fights numerous visual signs of skin aging caused by the negative effects of environmental factors. The skin looks smoother, smoother, younger and radiant, its tone is leveled. 8-hour antioxidant protection. 72-hour hydration. Strengthens the protective barrier of the skin after four hours. Light texture without oil content. Apply the serum to clean skin before moisturizer in the morning and evening. The volume of one pipette is sufficient for application. Gently spread the product on the skin of the face and neck.',
-    link: 'https://kaspi.kz/shop/p/estee-lauder-advanced-night-repair-syvorotka-dlja-litsa-50-ml-103071574/?c=750000000#!/item',
+    id: 6,
+    category: 2,
+    like: 15,
+    name: 'Apple iPhone 14 128Gb Blue Smartphone',
+    price: 420619,
+    description: 'iPhone 14 has the same superspeedy chip that\'s in iPhone 13 Pro. A15 Bionic, with a 5‑core GPU, powers all the latest features and makes graphically intense games and AR apps feel ultrafluid. An updated internal design delivers better thermal efficiency, so you can stay in the action longer.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
     rating: 'https://t4.ftcdn.net/jpg/02/74/86/43/360_F_274864312_uNlm9yCpdViwKzHkCp0sOBrmJFN0pKAa.jpg',
-    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h91/h2b/47573306540062/estee-lauder-advanced-night-repair-syvorotka-dla-lica-50-ml-103071574-1.jpg'
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h10/hd8/63073057407006/apple-iphone-14-128gb-goluboj-106363150-2.jpg'
+  },
+  {
+    id: 7,
+    category: 2,
+    like: 22,
+    name: 'Apple iPhone 13 128Gb Pink Smartphone',
+    price: 373599,
+    description: 'Apple has developed a completely new camera layout and deployed the lenses at 45 degrees. Thanks to this, the best system of two cameras with an enlarged matrix of a wide-angle camera fit inside the case. In addition, space was freed up for an optical image stabilization system with a matrix shift, and the speed of the matrix on an ultra-wide-angle camera increased. The new ultra-wide-angle camera sees more detail in the dark areas of the image. The ultra-wide angle camera also provides a large field of view, even when you are very close to the subject. So you don\'t have to step back to get everything you need into the frame.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-rozovyi-102298145/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he0/h12/46392661344286/apple-iphone-13-128gb-rozovyj-102298145-3-Container.jpg'
+  },
+  {
+    id: 8,
+    category: 2,
+    like: 4,
+    name: 'Apple iPhone 14 Pro Max 256Gb Purple Smartphone',
+    price: 689888,
+    description: 'Phone 14 Pro Max models look like the iPhone 13 Pro models with flat edges, stainless steel enclosure, a textured matte glass back, IP68 water resistance, and a Ceramic Shield-protected display, but the camera bumps are larger to accommodate new lenses, and the display has also changed',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item0',
+    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/had/h95/62948781293598/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-2.jpg'
+  },
+  {
+    id: 9,
+    category: 2,
+    like: 14,
+    name: 'Apple iPhone 13 128Gb Smartphone Blue',
+    price: 369500,
+    description: 'Apple iPhone 13 received a 6.1—inch Super Retina XDR display, which is characterized by an incredibly high pixel density - photos, videos and text look strikingly clear. And thanks to the reduced area of the True Depth camera, there is now more space for the image on the display. Apple has developed a completely new camera layout and deployed the lenses at 45 degrees. Thanks to this, the best system of two cameras with an enlarged matrix of a wide-angle camera fit inside the case. In addition, space was freed up for an optical image stabilization system with a matrix shift, and the speed of the matrix on an ultra-wide-angle camera increased. The new ultra-wide-angle camera sees more detail in the dark areas of the image. The ultra-wide angle camera also provides a large field of view, even when you are very close to the subject. So you don\'t have to step back to get everything you need into the frame.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-sinii-102298364/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h44/hfd/46392662130718/apple-iphone-13-128gb-sinij-102298364-3-Container.jpg'
   },
   {
     id: 10,
-    category: 1,
-    like: 6,
-    name: 'PANDORA Moments Ring 192312C01-56 17.5 4.76 g silver, zirconium',
-    price: 47900,
-    description: 'Pandora rings are exquisite handmade jewelry made of precious metals that will add "zest" to the image.',
-    link: 'https://kaspi.kz/shop/p/pandora-moments-192312c01-56-17-5-4-76-g-serebro-tsirkonii-106542091/?c=750000000#!/item',
+    category: 2,
+    like: 23,
+    name: 'Apple iPhone 11 128Gb Slim Box Smartphone White',
+    price: 299600,
+    description: 'The functional and stylish Apple iPhone 11 smartphone in a metal case is capable of providing not only everyday communication and entertainment, but also productive work. The image on the screen of a smartphone with a diagonal of 6.1 inches has amazing detail and color reproduction. Oleophobic coating eliminates heavy contamination of the screen. The iPhone 11 has a powerful 6-core A13 Bionic processor that performs any tasks quickly and smoothly. In addition, it works as energy efficient as possible, allowing you to save battery power. Hardware and software work together smoothly, so the battery charge is used with maximum efficiency. A non-removable battery with a capacity of 3110 mAh supports wireless charging, which will make this process easier and faster. The new dual camera system allows you to take photos with a wide and ultra-wide viewing angle. An updated interface and a new ultra-wide-angle camera will allow you to see and shoot what is happening outside the frame.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-belyi-100692385/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
     rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
-    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h88/62637913309214/pandora-moments-192312c01-56-17-5-4-76-g-serebro-cirkonij-106542091-3.jpg'
-  }
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3a/hb4/33208264228894/apple-iphone-11-128gb-slim-box-belyj-100692385-1-Container.jpg'
+  },
+  {
+    id: 11,
+    category: 3,
+    like: 1,
+    name: 'Acron Case for Apple iPhone 14 Plus Purple',
+    price: 1170,
+    description: 'A phone case is a cover that protects the outside of your phone and acts as a guard to your gadget against scratches, grime, and other risks. That means it protects your electronics and extends the longevity of the device as opposed to ending its lifespan early.',
+    link: 'https://kaspi.kz/shop/p/acron-dlja-apple-iphone-14-plus-fioletovyi-108054487/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://t4.ftcdn.net/jpg/02/74/86/43/360_F_274864312_uNlm9yCpdViwKzHkCp0sOBrmJFN0pKAa.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1d/heb/66918311133214/chekhol-acron-dlya-iphone-14-plus-soft-touch-fioletovyi-108054487-1.jpg'
+  },
+  {
+    id: 12,
+    category: 3,
+    like: 2,
+    name: 'Case Market Case for Apple iPhone 12 Multicolor',
+    price: 4550,
+    description: 'Type Case - with stand, Compatible - Apple Brand, Material - silicon , Features - Shockproof, Compatible models - of iPhone 12, The color of the cover - is a multicolored smile',
+    link: 'https://kaspi.kz/shop/p/chehol-market-dlja-apple-iphone-12-mnogotsvetnyi-108289253/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://t4.ftcdn.net/jpg/02/74/86/43/360_F_274864312_uNlm9yCpdViwKzHkCp0sOBrmJFN0pKAa.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h7b/h88/67479273734174/chekhol-market-dlya-apple-iphone-12-mnogotsvetnyi-108289253-1.jpg'
+  },
+  {
+    id: 13,
+    category: 3,
+    like: 20,
+    name: 'Acron Case for Apple iPhone 14 Red',
+    price: 2590,
+    description: 'Type - overlay,  Compatible -  Apple Brand, Material - silicon, Compatible models -  iPhone 14, The color of the case - is red',
+    link: 'https://kaspi.kz/shop/p/acron-dlja-apple-iphone-14-krasnyi-108054531/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://t4.ftcdn.net/jpg/02/74/86/43/360_F_274864312_uNlm9yCpdViwKzHkCp0sOBrmJFN0pKAa.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h71/hb7/66919070564382/chekhol-acron-dlya-iphone-14-soft-touch-krasnyi-108054531-1.jpg'
+  },
+  {
+    id: 14,
+    category: 3,
+    like: 7,
+    name: 'SeeDoo Defense Clear Case for iPhone 14 Pro. Transparent',
+    price: 989,
+    description: 'A phone case is a cover that protects the outside of your phone and acts as a guard to your gadget against scratches, grime, and other risks. That means it protects your electronics and extends the longevity of the device as opposed to ending its lifespan early.',
+    link: 'https://kaspi.kz/shop/p/chehol-seedoo-defense-clear-case-dlja-iphone-14-pro-prozrachnyi-107118653/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc3/h61/63805367812126/chekhol-seedoo-defense-clear-case-dlya-iphone-14-pro-prozrachnyi-107118653-1.jpg'
+  },
+  {
+    id: 15,
+    category: 3,
+    like: 21,
+    name: 'Royal Touch Case Paris Case under Varnish for Iphone 14',
+    price: 13000,
+    description: 'A phone case is a cover that protects the outside of your phone and acts as a guard to your gadget against scratches, grime, and other risks. That means it protects your electronics and extends the longevity of the device as opposed to ending its lifespan early.',
+    link: 'https://kaspi.kz/shop/p/royal-touch-chehol-parizh-pod-lakom-na-iphone-14-108110211/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAxvGfBhB-EiwAMPakqhMMHJi5RD8JbMGmPAtXLDzHJWO1gKu3ELRLt6IIsnK5NqypIlS4hxoC1aAQAvD_BwE#!/item',
+    rating: 'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb0/h93/67039926747166/royal-touch-dlya-apple-iphone-13-pro-mul-tikolor-108110211-1.jpg'
+  },
 ];
 
 

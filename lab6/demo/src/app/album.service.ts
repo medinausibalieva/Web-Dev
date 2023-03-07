1e06a287ba9235189581c08db6b897370f0de147
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-03-07 19:46:01 +0600

    added several functions

--- lab6/demo/src/app/album.service.ts
+++ lab6/demo/src/app/album.service.ts
@@ -7,10 +7,18 @@ import {Album} from "./models";
   providedIn: 'root'
 })
 export class AlbumService {
-
+  BASE_URL: string = 'https://jsonplaceholder.typicode.com';
   constructor(private client: HttpClient) { }
 
   getAlbums(): Observable<Album[]>{
-    return this.client.get<Album[]>('https://jsonplaceholder.typicode.com/albums')
+    return this.client.get<Album[]>(`${this.BASE_URL}/albums`)
+  }
+
+  getAlbum(id: number): Observable<Album>{
+    return this.client.get<Album>(`${this.BASE_URL}/albums/${id}`)
+  }
+
+  addAlbum(album:Album) : Observable<Album>{
+    return this.client.post<Album>(`${this.BASE_URL}/albums`, album);
   }
 }

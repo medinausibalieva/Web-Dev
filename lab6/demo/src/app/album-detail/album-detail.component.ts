1171023bf6ff5f591356962beed368bf96acebca
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-03-07 17:51:17 +0600

    added boolean loaded

--- lab6/demo/src/app/album-detail/album-detail.component.ts
+++ lab6/demo/src/app/album-detail/album-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ALBUMS} from "../fake-db";
 import {Album} from "../models";
+import {AlbumService} from "../album.service";
 
 
 @Component({
@@ -11,13 +12,23 @@ import {Album} from "../models";
 })
 export class AlbumDetailComponent implements OnInit{
   album: Album;
-  constructor(private route: ActivatedRoute) {
+  loaded: boolean;
+  constructor(private route: ActivatedRoute,
+              private albumService: AlbumService) {
     this.album = {} as Album;
+    this.loaded = true;
   }
 
   ngOnInit() : void{
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-
-    this.album = ALBUMS.find((album:Album) => album.id === id) as Album;
+    // const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.loaded = false;
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      // this.album = ALBUMS.find((album:Album) => album.id === id) as Album;
+      this.albumService.getAlbum(id).subscribe((album) => {
+        this.album = album;
+        this.loaded = true;
+      });
+    })
   }
 }

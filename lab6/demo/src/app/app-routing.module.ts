baaa95dbdd3313f6f2ef6ad23917234c511dab6e
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-03-08 10:48:19 +0600

    Added path album/photo

--- lab6/demo/src/app/app-routing.module.ts
+++ lab6/demo/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {AboutComponent} from "./about/about.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 import {AlbumsComponent} from "./albums/albums.component";
 import {AlbumDetailComponent} from "./album-detail/album-detail.component";
+import {AlbumPhotosComponent} from "./album-photos/album-photos.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'albums', component: AlbumsComponent},
   {path: 'album/:id', component: AlbumDetailComponent},
+  {path: 'albums/:id/photos', component: AlbumPhotosComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
 ];

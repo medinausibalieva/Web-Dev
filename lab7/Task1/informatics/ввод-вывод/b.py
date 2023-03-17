9bf80ebb3dcc003f5965d19618b8ba6a2df6dae7
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-03-17 21:44:16 +0600

    informatics tasks

---
+++ lab7/Task1/informatics/ввод-вывод/b.py
@@ -0,0 +1,6 @@
+a = int(input())
+
+next = 'The next number for the number {} is {}.'.format(a, a + 1)
+prev = 'The previous number for the number {} is {}.'.format(a, a - 1)
+print(next)
+print(prev)
---
+++ lab7/Task1/informatics/условный оператор/d.py
@@ -0,0 +1,8 @@
+a = int(input())
+
+def sign(x):
+    if x > 0: return 1
+    elif x < 0: return -1
+    else: return 0
+
+print(sign(a))

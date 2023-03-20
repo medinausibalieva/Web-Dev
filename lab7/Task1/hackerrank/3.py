8f86fd4759192f43aed8e61c84b06f83bdfc8f5d
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-03-20 20:35:44 +0600

    solved 10 problems in harrerank

---
+++ lab7/Task1/hackerrank/3.py
@@ -0,0 +1,7 @@
+# Python: Divison
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
---
+++ lab7/Task1/hackerrank/7.py
@@ -0,0 +1,7 @@
+# Find the runner-up score
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    print(sorted(set(arr), reverse=True)[1])

cc86ed693f9116e863e0e563b4aa4b235fdf1d1c
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-04-09 13:19:16 +0600

    created models vacancy, company

--- lab9/hh_back/api/models.py
+++ lab9/hh_back/api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField
+    city = models.CharField
+    address = models.TextField
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+    def to_json(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+        }
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField
+    salary = models.FloatField(default=150000)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Vacancy'
+        verbose_name_plural = 'Vacancies'
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary
+        }

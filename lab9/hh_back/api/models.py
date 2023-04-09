498ed8f672411b3286bf5a98bea60dec9a4feff3
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-04-09 13:30:58 +0600

    created models vacancy, company

--- lab9/hh_back/api/models.py
+++ lab9/hh_back/api/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
-# Create your models here
+# Create your models here.
+
 class Company(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField
-    city = models.CharField
-    address = models.TextField
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    city = models.CharField(max_length=50)
+    address = models.TextField()
 
     class Meta:
         verbose_name = 'Company'
         verbose_name_plural = 'Companies'
 
     def __str__(self):
-        return f'{self.id}: {self.name}'
-
+        return f'{self.id}: {self.name}, {self.city}, {self.address}'
+    
     def to_json(self):
-        return{
+        return {
             'id': self.id,
             'name': self.name,
             'description': self.description,
             'city': self.city,
             'address': self.address
         }
-
+    
 class Vacancy(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField
-    salary = models.FloatField(default=150000)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    salary = models.FloatField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     class Meta:
@@ -34,12 +35,13 @@ class Vacancy(models.Model):
         verbose_name_plural = 'Vacancies'
 
     def __str__(self):
-        return f'{self.id}: {self.name}'
-
+        return f'{self.id}: {self.name}, {self.salary}, {self.company}'
+    
     def to_json(self):
         return {
             'id': self.id,
             'name': self.name,
             'description': self.description,
-            'salary': self.salary
+            'salary': self.salary,
+            'company': self.company.name
         }

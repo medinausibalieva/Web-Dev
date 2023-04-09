aa875af54725334d9982c807328724abbc4087f6
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-04-09 22:57:52 +0600

    updated admin.py

--- lab9/hh_back/api/admin.py
+++ lab9/hh_back/api/admin.py
@@ -3,5 +3,12 @@ from .models import Company, Vacancy
 
 # Register your models here.
 
-admin.site.register(Company)
-admin.site.register(Vacancy)
+# admin.site.register(Company)
+# admin.site.register(Vacancy)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'city')
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'salary')
+

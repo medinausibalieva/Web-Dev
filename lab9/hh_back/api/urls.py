89badbeac94bb5b21e8431b4acafacb84148a2f7
medinausibalieva <98639460+medinausibalieva@users.noreply.github.com>
2023-04-09 23:35:37 +0600

    added corrections

--- lab9/hh_back/api/urls.py
+++ lab9/hh_back/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/companies/', views.company_list),
-    path('api/companies/<int:id>/', views.company_detail),
-    path('api/companies/<int:id>/vacancies/', views.company_vacancies),
-    path('api/vacancies/', views.vacancy_list),
-    path('api/vacancies/<int:id>/', views.vacancy_detail),
-    path('api/vacancies/top_ten/', views.vacancy_top_ten),
+    path('companies/', views.company_list),
+    path('companies/<int:id>/', views.company_detail),
+    path('companies/<int:id>/vacancies/', views.company_vacancies),
+    path('vacancies/', views.vacancy_list),
+    path('vacancies/<int:id>/', views.vacancy_detail),
+    path('vacancies/top_ten/', views.vacancy_top_ten),
 ]
